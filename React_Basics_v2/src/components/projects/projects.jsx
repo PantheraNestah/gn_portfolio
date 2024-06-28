1bf8fe4b16687efd4744280a8f502f208f07fc84
PantheraNestah <pantheranestah@gmail.com>
2024-06-28 12:42:01 +0300

    adding polluwatch photo and link

--- React_Basics_v2/src/components/projects/projects.jsx
+++ React_Basics_v2/src/components/projects/projects.jsx
@@ -17,11 +17,13 @@ const Projects = () => {
 }
 
 
-const Project = ({title, description, stack}) => {
+const Project = ({title, img, link, description, stack}) => {
     
     return (
         <article className="project d-flex align-items-start justify-content-center">
-            <div className="project_photo 6"></div>
+            <div className="project_photo border">
+                <img src={img} alt="" />
+            </div>
             <div className="project_details col-lg-5 d-flex flex-column align-items-start">
                 <h3>{title}</h3>
                 <p className="">{description}</p>
@@ -30,7 +32,7 @@ const Project = ({title, description, stack}) => {
                         return <li key={index}>{item},</li>
                     })}
                 </ul>
-                <a href="#" className="view_proj_link">View project</a>
+                <a href={link} target="_blank" className="view_proj_link">View project</a>
             </div>
         </article>
     );
